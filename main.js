6713968ebdbdab9a14adb529f6564f9f68b9e01c
Shadow Ma <i@shadow.ma>
2012-11-22 00:11:22 +0800

    change time format

--- main.js
+++ main.js
@@ -8,19 +8,28 @@ function onShare(){
     title = Amarok.Engine.currentTrack().title;
     length = Amarok.Engine.currentTrack().length;    
     
-    second = length/1000;
-    minute = second / 60;
-    second = second % 60;
-    duration = Math.floor(minute) + ":" + Math.floor(second);
+    t = new Date(length);
+    if (length/1000 > 3600) {
+      duration = t.getHours() + ':' + checkTime(t.getMinutes()) + ':' + checkTime(t.getSeconds());
+    }
+    else {
+      duration = t.getMinutes() + ':' + checkTime(t.getSeconds());
+    }  
     
     d = new Date();
-    playdate = d.getFullYear() + '-' +(d.getMonth()+1) + '-' + d.getDate() + ' ' + d.getHours() + ':' + d.getMinutes() + ':' + d.getSeconds();
-    surl = 'http://music.wayshine.us/nowplaying.php?album='+album+'&artist='+artist+'&title='+title+'&length='+duration+'&date='+playdate+'&from=623&c=0&s=0';
+    playdate = d.getFullYear() + '-' +checkTime((d.getMonth()+1)) + '-' + checkTime(d.getDate()) + ' ' + checkTime(d.getHours()) + ':' + checkTime(d.getMinutes()) + ':' + checkTime(d.getSeconds());
     
+    surl = 'http://music.wayshine.us/nowplaying.php?album='+album+'&artist='+artist+'&title='+title+'&length='+duration+'&date='+playdate+'&from=623&c=0&s=0';
     Url = new QUrl(surl);
     QDesktopServices.openUrl( Url );
 }
 
+function checkTime(i){
+  if (i<10){
+    i="0" + i;
+  }
+  return i;
+}
+
 Amarok.Window.addToolsMenu("share", "Share Nowplaying");
 Amarok.Window.ToolsMenu.share['triggered()'].connect( onShare );
-
